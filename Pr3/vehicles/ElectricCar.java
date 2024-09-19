ce2de5012a2bff590b3f4b2cb1a2a8a162438159
Gwynbleidd <164669786+Gwynbleidd0241@users.noreply.github.com>
2024-09-19 10:15:29 +0300

    Добавили еще один класс

---
+++ Pr3/vehicles/ElectricCar.java
@@ -0,0 +1,19 @@
+package vehicles;
+
+public class ElectricCar extends Car{
+
+    private int batteryCapacity;
+
+    public ElectricCar(String ownerName, String insuranceNumber, int batteryCapacity) {
+        super(ownerName, insuranceNumber, "Electric");
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    public int getBatteryCapacity() {
+        return batteryCapacity;
+    }
+
+    public void setBatteryCapacity(int batteryCapacity) {
+        this.batteryCapacity = batteryCapacity;
+    }
+}
